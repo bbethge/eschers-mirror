4229e9952ef92221de87adee550cc9c756394731
Ben Bethge <brby8b@mst.edu>
2009-07-22 03:17:59 -0500

    Attempted to make build script more portable

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from distutils.core import setup, Extension
+import subprocess
+
+includes = []
+defines = []
+libs = ['xine']
+lib_dirs = []
+sdlConfig = subprocess.Popen(
+	['sdl-config', '--libs', '--cflags'], stdout=subprocess.PIPE,
+	stderr=subprocess.STDOUT
+	).communicate()[0]
+for opt in sdlConfig.split():
+	if opt[:2] == '-D':
+		eqIdx = opt.find('=')
+		if eqIdx >= 0:
+			defines += [ (opt[2:eqIdx], opt[eqIdx:]) ]
+		else:
+			defines += [ (opt[2:], None) ]
+	elif opt[:2] == '-I':
+		includes += [opt[2:]]
+	elif opt[:2] == '-l':
+		libs += [opt[2:]]
+	elif opt[:2] == '-L':
+		lib_dirs += [opt[2:]]
 
 VideoDecode = Extension(
 	'VideoDecode',
-	include_dirs = ['/usr/include/SDL'],  # TODO: portability
-	define_macros = [('_REENTRANT', None), ('_GNU_SOURCE', 1)],
-	libraries = ['xine', 'SDL'],
+	include_dirs = includes,
+	define_macros = defines,
+	libraries = libs,
 	sources = ['VideoDecode.c'])
 
 setup(
